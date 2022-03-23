6f3b7697556389da68b90f215d3022df96f8dfa8
muhammetyavuz123 <muhametyavuz@hotmail.com>
2022-03-23 17:14:29 +0300

    eth dapp

---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  solidity: "0.8.4",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/39f882d41b3a47a5b5dfdb69d0fd209e",
+      accounts: [
+        `ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`,
+      ],
+    },
+  },
+};
